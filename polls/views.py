c59c3483425f28330107a60663dc8e4aa309c122
chris129 <chrisli129@gmail.com>
2019-08-15 17:27:48 +0800

    index html 里面删除硬编码

--- polls/views.py
+++ polls/views.py
@@ -6,26 +6,15 @@ from django.template import loader
 from django.shortcuts import get_object_or_404, render
 # Create your views here.
 def index(request):
-    #return HttpResponse("Hello, world. You're at the polls index.")
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
 
-    #output = ', '.join([q.question_text for q in latest_question_list])
-    #return HttpResponse(output)
     context = {
         'latest_question_list': latest_question_list,
     }
-    #template = loader.get_template('polls/index.html')
-    #return HttpResponse(template.render(context, request)) 我们不再需要导入 loader和 HttpResponse，而是从django.shortcuts导入了render等价于下面的render
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    #try+except ===
     question = get_object_or_404(Question, pk=question_id)
-    #return HttpResponse("You're looking at question %s." % question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
